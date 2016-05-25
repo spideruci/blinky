21d86df2970c211dd82ffc980610c74b6d816803
Vijay Krishna Palepu <vpalepu@uci.edu>
2016-05-25 14:47:19 -0700

    Removing redundant files in Blinky Diagnostics

--- blinky-diagnostics/src/main/java/org/spideruci/analysis/diagnostics/App.java
+++
@@ -1,8 +0,0 @@
-package org.spideruci.analysis.diagnostics;
-
-
-public class App {
-    public static void main( String[] args ) {
-        throw new UnsupportedOperationException("TODO");
-    }
-}
