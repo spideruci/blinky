496f2436354058b07dd46211333405f0115bfbb9
Vijay Krishna Palepu <vpalepu@uci.edu>
2016-05-25 14:46:06 -0700

    Preliminary Diagnostics Script

---
+++ blinky-diagnostics/src/test/java/org/spideruci/analysis/diagnostics/SubjectRunner.java
@@ -0,0 +1,16 @@
+package org.spideruci.analysis.diagnostics;
+
+import org.junit.Test;
+
+public class SubjectRunner {
+  
+  public static void main(String[] args) {
+    new SubjectRunner().run();
+  }
+  
+  @Test
+  public void run() {
+    
+  }
+
+}
