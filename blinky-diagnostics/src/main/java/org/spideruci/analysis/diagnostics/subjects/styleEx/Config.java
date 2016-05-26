eddade8d24ffce19edc5e610e51058b031713c1a
Vijay Krishna Palepu <vpalepu@uci.edu>
2016-05-25 21:44:42 -0700

    Adding additional subjects, input and configs to diagnosis - also: moved exec'n of diagnosis script to mvn:verify

--- blinky-diagnostics/src/main/java/org/spideruci/analysis/diagnostics/subjects/styleEx/Config.java
+++ blinky-diagnostics/src/main/java/org/spideruci/analysis/diagnostics/subjects/styleEx/Config.java
@@ -6,6 +6,6 @@
 package org.spideruci.analysis.diagnostics.subjects.styleEx;
 
 public class Config {
-  public final static String bookPath = "pride-and-prejudice.txt";
+  public final static String bookPath = "FOUNTAIN-5-2-16-nyt.txt";
   public final static String stopWordsPath = "stop_words.txt"; 
 }
