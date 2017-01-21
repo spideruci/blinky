38c6019c63d1098e46082bb9f36156a337a13928
jycamus90 <jycamus90@gmail.com>
2017-01-21 14:24:52 -0800

    Added a variable in AnalysisBlock.java
    AnalysisBlock.java
     - added a variable ‘subject’. This stores the name of the subject
    project that we are analyzing. Based on this variable, we will filter
    out dependency libraries.

--- blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/AnalysisBlock.java
+++ blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/AnalysisBlock.java
@@ -3,7 +3,15 @@ package org.spideruci.analysis.statik.blocks;
 import org.spideruci.analysis.statik.calls.CallGraphManager;
 
 public abstract class AnalysisBlock {
-  
+  private String subject;
+	
   public abstract void execute(CallGraphManager cgm);
 
+  public void setSubject(String s){
+	  subject = s;
+  }
+  
+  protected String getSubject(){
+	  return subject;
+  }
 }
