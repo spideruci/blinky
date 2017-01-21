4d181aa13a2d8fd4da957ba8d02ac15c4d724ccd
jycamus90 <jycamus90@gmail.com>
2017-01-21 14:34:53 -0800

    Modified FlowBlock
    Modified FlowBlock

--- blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/FlowBlock.java
+++ blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/FlowBlock.java
@@ -1,5 +1,6 @@
 package org.spideruci.analysis.statik.blocks;
 
+import org.spideruci.analysis.io.WriteToText;
 import org.spideruci.analysis.statik.calls.CallGraphManager;
 import org.spideruci.analysis.statik.controlflow.Graph;
 import org.spideruci.analysis.statik.flow.StatikFlowGraph;
@@ -11,23 +12,38 @@ public class FlowBlock extends AnalysisBlock {
 
   @Override
   public void execute(CallGraphManager cgm) {
+	  
+//	  cgm.getCallgraph().toString();
+	  
+	WriteToText wtt = new WriteToText();
+	
+//	wtt.dump(cgm);
+	  
     StatikFlowGraph flowGraph = StatikFlowGraph.init(cgm);
     
     System.out.println("---- I C F G ----");
     
-    flowGraph.buildIcfg();
-    Graph<Unit> icfg = flowGraph.getIcfg();
-    System.out.println(icfg);
+    flowGraph.buildIcfg(this.getSubject());
+    
+//    wtt.dump(flowGraph.icfgMgr.icfg());
+    
+//    Graph<Unit> icfg = flowGraph.getIcfg();
+//    System.out.println(icfg);
         
     System.out.println("---- Java Source Icfg ----");
     
-    Graph<String> icfgGraph = flowGraph.icfgMgr.icfgJavaSourceLines();
-    System.out.println(icfgGraph);    
+    Graph<String> icfgGraph = flowGraph.icfgMgr.icfgJavaSourceLines(this.getSubject());
+//    System.out.println(icfgGraph);    
     
-//    System.out.println("---- Writing to Database ----");
-//  
+//    wtt.dump(icfgGraph);
+
+    
+//    icfg = null;
+    System.gc();
+    
+    System.out.println("---- Writing to Database ----");  
     SourceICFG sIcfg = new SourceICFG(icfgGraph);
-    sIcfg.dumpData();
+    sIcfg.dumpData(this.getSubject());
     
     System.out.println("Done!");
     
