68ecde7e09337b1cfa9ba24960e3d19d40d72512
jycamus90 <jycamus90@gmail.com>
2017-01-21 14:31:21 -0800

    Disable printing in CallGraphBlock.java

--- blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/CallGraphBlock.java
+++ blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/CallGraphBlock.java
@@ -1,7 +1,9 @@
 package org.spideruci.analysis.statik.blocks;
 
+import org.spideruci.analysis.statik.Items;
 import org.spideruci.analysis.statik.calls.CallGraphManager;
 
+import soot.MethodOrMethodContext;
 import soot.SootMethod;
 
 public class CallGraphBlock extends AnalysisBlock {
@@ -9,26 +11,37 @@ public class CallGraphBlock extends AnalysisBlock {
   @Override
   public void execute(CallGraphManager cgm) {
 
-    System.out.println("----");
+//    System.out.println("----");
+//
+//    int i = 0;
+//    for(SootMethod method : cgm.getBfsTraversal()) {
+//      System.out.println(method);
+//      i += 1;
+//    	if(method.getName().contains("test_isPrecise")){
+//    		System.out.println(method);
+//    	}
+    	
+//    }
+//    System.out.println(i);
 
-    int i = 0;
-    for(SootMethod method : cgm.getBfsTraversal()) {
-      System.out.println(method);
-      i += 1;
-    }
-    System.out.println(i);
-
-    System.out.println("----");
-    int j = 0;
-    for(SootMethod method : cgm.getBottomupTopology()) {
-      j += 1;
-      System.out.println(method);
-    }
-    System.out.println(j);
+//    System.out.println("----");
+//    int j = 0;
+//    for(SootMethod method : cgm.getBottomupTopology()) {
+//      j += 1;
+//      System.out.println(method);
+//    }
+//    System.out.println(j);
 
     System.out.println("---- Call Graph ----");
 
-    cgm.printGraph();
+//    cgm.printGraph();
+    
+    for(MethodOrMethodContext m: Items.items(cgm.getCallgraph().sourceMethods())){
+    	if(m == null)
+    		continue;
+    	
+    	System.out.println(m);
+    }
 
   }
 
