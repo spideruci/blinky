f42b0f75387db0d18b27bc411145e6379a8a22ba
Vijay Krishna Palepu <vpalepu@uci.edu>
2020-10-15 14:23:53 -0700

    Archiving

--- blinky-statik/src/main/java/org/spideruci/analysis/statik/blocks/AnalysisBlock.java
+++
@@ -1,9 +0,0 @@
-package org.spideruci.analysis.statik.blocks;
-
-import org.spideruci.analysis.statik.calls.CallGraphManager;
-
-public abstract class AnalysisBlock {
-  
-  public abstract void execute(CallGraphManager cgm);
-
-}
